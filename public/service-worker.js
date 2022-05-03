d95d9638ff3e4c3207753d11adb53d2239995ab5
abdisalamhassan <95972020+abdisalamhassan@users.noreply.github.com>
2022-05-03 18:33:45 -0400

    add service worker

---
+++ public/service-worker.js
@@ -0,0 +1,19 @@
+const APP_PREFIX = "BudgetTracker-";
+const VERSION = "version_01";
+const CACHE_NAME = APP_PREFIX + VERSION;
+
+const FILES_TO_CACHE = [
+    "./index.html",
+    "./css/styles.css",
+    "./js/index.js",
+    "./manifest.json",
+    "./icons/icons-72x72.png",
+    "./icons/icons-96x96.png",
+    "./icons/icons-128x128.png",
+    "./icons/icons-144x144.png",
+    "./icons/icons-152x152.png",
+    "./icons/icons-192x192.png",
+    "./icons/icons-384x384.png",
+    "./icons/icons-512z512.png",
+];
+
