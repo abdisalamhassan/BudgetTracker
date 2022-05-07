c30fd71f5705e896645cc936478714168e7c1e54
abdisalamhassan <95972020+abdisalamhassan@users.noreply.github.com>
2022-05-07 19:26:43 -0400

    service worker

--- public/service-worker.js
+++ public/service-worker.js
@@ -17,3 +17,56 @@ const FILES_TO_CACHE = [
     "./icons/icons-512z512.png",
 ];
 
+self.addEventListener('install', evt => {
+    evt.waitUntil(
+        caches.open(CACHE_NAME).then(cache => {
+            console.log('Your files are pre-cached succesfully');
+            return cache.addAll(Files_To_CACHE);
+        })
+        );
+        self.skipWaiting();
+});
+
+self.addEventListener('activate', evt => {
+    evt.WaitUntil(
+        caches.keys().then(keyList => {
+            return Promise.all(
+                keyList.map( key => {
+                    if (key !== CACHE_NAME && key !== DATA_CACHE_NAME){
+                        console.log('Removing old cache data', key);
+                        return caches.delete(key);
+                    }
+                })
+            );
+        })
+    )
+    self.clients.claim();
+});
+
+self.addEventListener('fetch', evt => {
+    if(evt.request.url.includes('/api')){
+        evt.respondWith(
+            caches.open(DATA_CACHE_NAME).then(cache => {
+                return fetch(evt.request)
+                .then(response => {
+                    if (response.status === 200 ){
+                        cache.put(evt.request.url, response.clone());
+                    }
+                    return response;
+                })
+                .catch(err => {
+                    return cache.match(evt.request);
+                });
+            })
+        );
+        return;
+    }
+
+    evt.respondWith(
+        caches.open(CACHE_NAME).then( cache => {
+            return cache.match(evt.request).then(response => {
+                return response || fetch(evt.request)
+            });
+        })
+    )
+})
